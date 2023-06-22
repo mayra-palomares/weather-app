7925df24db160a7a17ba3034ad711cd88e41aff1
Mayra Palomares <mayra.palomares.delacruz@gmail.com>
2023-06-22 18:59:46 +0100

    Add search input

---
+++ src/app/components/SearchInput.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+type Props = {
+  onClick: () => void;
+};
+
+const SearchInput = ({ onClick }: Props) => {
+  return (
+    <div className="search">
+      <input
+        type="text"
+        name="city"
+        id="city"
+        placeholder="Search city"
+        autoComplete="off"
+      />
+      <button type="submit" onClick={onClick}>
+        <svg
+          aria-hidden="true"
+          className="w-5 h-5"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          ></path>
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default SearchInput;
--- src/app/components/WeatherData.tsx
+++ src/app/components/WeatherData.tsx
@@ -3,15 +3,21 @@ import { CurrentWeather as CurrentWeatherType } from "../types/CurrentWeather";
 import { ForecastWeather as ForecastWeatherType } from "../types/ForecastWeather";
 import CurrentWeather from "./CurrentWeather";
 import ForecastWeather from "./ForecastWeather";
+import SearchInput from "./SearchInput";
 
 type Props = {
   current: CurrentWeatherType;
   forecast: ForecastWeatherType;
 };
 
+const searchWeatherInfo = () => {
+  console.log("Click on Search");
+};
+
 const WeatherData = ({ current, forecast }: Props) => {
   return (
     <div className="weatherPage">
+      <SearchInput onClick={searchWeatherInfo} />
       <CurrentWeather data={current} />
       <ForecastWeather data={forecast} />
     </div>
